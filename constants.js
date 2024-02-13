feb5c87744fc802c95b3eba28c88dceee09ff2f1
satyamco <skumars261141@gmail.com>
2024-02-13 12:39:02 +0530

    first commit

---
+++ constants.js
@@ -0,0 +1,81 @@
+
+export const heroText = {
+    heading: "FrontEnd web Developer",
+    para: "Passionate about crafting innovative web solutions with a focus on user experience and performance.",
+    btnText: "My Github"
+}
+
+export const technologies ={
+    heading: "I seamlessly blend HTML, CSS, JavaScript, and React to craft dynamic and scalable web solutions.",
+    techno: ['HTML', 'CSS', 'JavaScript', 'React']
+}
+
+export const Projects = {
+    heading: 'Explore my recent Projects',
+    Project:[
+        {
+            img:"",
+            category:"",
+            heading:"",
+            para:"",
+            github:"",
+            live:""
+        },
+        {
+            img:"",
+            category:"",
+            heading:"",
+            para:"",
+            github:"",
+            live:""
+        },
+        {
+            img:"",
+            category:"",
+            heading:"",
+            para:"",
+            github:"",
+            live:""
+        },
+        {
+            img:"",
+            category:"",
+            heading:"",
+            para:"",
+            github:"",
+            live:""
+        },
+        {
+            img:"",
+            category:"",
+            heading:"",
+            para:"",
+            github:"",
+            live:""
+        },
+    ]
+
+}
+
+export const connect ={
+    heading:"Feel free to get in touch. I'm always open to new opportunities and collaborations. I'm just an email away from turning your ideas into reality.",
+    btnText:"Get In Touch"
+}
+
+export const footer = {
+    logo: "Satyam K.",
+    links:[
+        {
+            linkText: "Email",
+            src:"mailto:mail.satyamm@gmail.com"
+        },
+        {
+            linkText: "Linkedin",
+            src:"www.linkedin.com/satyamco"
+        },
+        {
+            linkText: "Github",
+            src:"www.github.com/satyamco"
+        }
+    ]
+}
